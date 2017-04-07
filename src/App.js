d418c0a4457f81f6909a94fb83c78b2478bd9c0d
gcc17 <gcc17@case.edu>
2017-04-07 18:54:31 -0400

    Fixed bug
    Signed-off-by: gcc17 <gcc17@case.edu>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import logo from './logo.svg';
-import MAPBOX_KEY from "../mapboxkey.js"
+//import { MAPBOX_KEY } from "../mapboxkey.js"
 import './App.css';
 
 class App extends Component {
+
   state = {
-    lat: 41.487007,
-    lng: -81.504302,
+    lat: 51.505,
+    lng: -0.09,
     zoom: 13,
   }
-
-/*
-url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
-attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
-maxZoom: 18
-id: 'mapbox/streets-v10/tiles/256'
-accessToken: MAPBOX_KEY
-*/
-
 
   render () {
     const position = [this.state.lat, this.state.lng]
@@ -36,6 +28,13 @@ accessToken: MAPBOX_KEY
       </Map>
     )
   }
+/*
+url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+maxZoom: 18
+id: 'mapbox/streets-v10/tiles/256'
+accessToken: MAPBOX_KEY
+*/
 }
 
 export default App;
