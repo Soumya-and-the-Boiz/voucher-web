794115bfaea863d0507fc6f63ba6a17caeaa9bd9
gcc17 <gcc17@case.edu>
2017-04-07 19:24:04 -0400

    Got the mapbox map working
    Signed-off-by: gcc17 <gcc17@case.edu>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import logo from './logo.svg';
-import MAPBOX_KEY from "./mapboxkey.js"
+import { MAPBOX_KEY } from "./mapboxkey.js"
 import './App.css';
 
 class App extends Component {
 
   state = {
-    lat: 51.505,
-    lng: -0.09,
+    lat: 41.498545,
+    lng: -81.689281,
     zoom: 13,
   }
 
   render () {
+    console.log(MAPBOX_KEY)
     const position = [this.state.lat, this.state.lng]
+    const mapboxURL = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=' + MAPBOX_KEY
     return (
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
           attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          url={mapboxURL}
         />
         <Marker position={position}>
           <Popup>
@@ -29,7 +31,10 @@ class App extends Component {
     )
   }
 /*
-url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+
+https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{level}/{col}/{row}@2x?access_token=pk.eyJ1IjoiZ2NjYWxkd2VsbCIsImEiOiJjajE4YzkyZ2kwNm93MzJvN2Rqa3M2djB6In0.ld3nbtPiF2xzW863YOt2QQ
+
+url='https://api.tiles.mapbox.com/v4/mapbox/streets-v10/tiles/256/{z}/{x}/{y}.png?access_token=' + MAPBOX_KEY
 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
 maxZoom: 18
 id: 'mapbox/streets-v10/tiles/256'
