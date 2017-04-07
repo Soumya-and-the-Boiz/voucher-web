3c488cda774dab81f8b8a9f7b6e864653af84d2a
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-04-07 19:25:12 -0400

    Add simple overlay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import logo from './logo.svg';
-import MAPBOX_KEY from "../mapboxkey.js"
 import './App.css';
 
 class App extends Component {
@@ -23,17 +22,20 @@ accessToken: MAPBOX_KEY
   render () {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
-          </Popup>
-        </Marker>
-      </Map>
+      <div className="app-root">
+        <Map className="map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
+        <div className="panels">MOSMDFOASDMFAOSDMFASODFMASDOF</div>
+      </div>
     )
   }
 }
