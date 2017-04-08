167f3d0dc1e6a4ef30a70279a60b34030ac2322a
gcc17 <gcc17@case.edu>
2017-04-07 22:37:01 -0400

    Polygon components now render from list of tracts in state
    Signed-off-by: gcc17 <gcc17@case.edu>

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
       lat: 41.498545,
       lng: -81.689281,
       zoom: 13,
-      markers: []
+      markers: [],
+      tracts: [{positions: [[41.467714, -81.759736], [41.467195, -81.758724], [41.465218, -81.758711], [41.465238, -81.758649], [41.466693, -81.754202], [41.467728, -81.751035], [41.468388, -81.749038], [41.468464, -81.748835], [41.469226, -81.746722], [41.47009, -81.745592], [41.470056, -81.751034], [41.47046, -81.748568], [41.472142, -81.748559], [41.473252, -81.748558], [41.477114, -81.748543], [41.477103, -81.750968], [41.477066, -81.754133], [41.47043, -81.754186], [41.470107, -81.75286], [41.469819, -81.752498], [41.46878, -81.755502], [41.467997, -81.758053], [41.467714, -81.759736]],
+      key: 0}]
     }
   }
 
@@ -46,6 +48,13 @@ class App extends Component {
         </Popup>
       </Marker>
     ));
+    const Tracts = this.state.tracts.map(tract => (
+      <Polygon positions={positions} key={tract.key}>
+        <Popup>
+          <span>I AM A TRACT!</span>
+        </Popup>
+      </Polygon>
+    ));
     return (
       <Map center={position} onClick={this.addMarker.bind(this)} zoom={this.state.zoom}>
         <TileLayer
@@ -53,11 +62,7 @@ class App extends Component {
           url={mapboxURL}
         />
         {Markers}
-        <Polygon positions={positions}>
-          <Popup>
-            <span>I am a tract! Check my sexy body out!</span>
-          </Popup>
-        </Polygon>
+        {Tracts}
       </Map>
     )
   }
