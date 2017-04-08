5317b469998fcf1e4315c605e5168378ef9fc7f8
Billy Barbaro <billy@thebarbaros.com>
2017-04-08 11:44:44 -0400

    Zooming on div clicks.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ class App extends Component {
       markers: [],
       tracts: [{'tract' : {
             'name': 'Tract 1',
+            'center_lat': '41.471166',
+            'center_lng': '-81.75266400000001',
             'bounding_rect': [[41.467714, -81.759736], [41.467195, -81.758724], [41.465218, -81.758711], [41.465238, -81.758649], [41.466693, -81.754202], [41.467728, -81.751035], [41.468388, -81.749038], [41.468464, -81.748835], [41.469226, -81.746722], [41.47009, -81.745592], [41.470056, -81.751034], [41.47046, -81.748568], [41.472142, -81.748559], [41.473252, -81.748558], [41.477114, -81.748543], [41.477103, -81.750968], [41.477066, -81.754133], [41.47043, -81.754186], [41.470107, -81.75286], [41.469819, -81.752498], [41.46878, -81.755502], [41.467997, -81.758053], [41.467714, -81.759736]],
             'img_src': 'https://s-media-cache-ak0.pinimg.com/736x/73/de/32/73de32f9e5a0db66ec7805bb7cb3f807.jpg',
             'education_rank': '42',
@@ -29,6 +31,14 @@ class App extends Component {
     }
   }
 
+  zoomToCoordinates(lat, lng) {
+    this.setState({
+      lat: lat,
+      lng: lng,
+      zoom: 14
+    });
+  }
+
   addMarker(data) {
     console.log(data);
     var oldMarkers = this.state.markers
@@ -37,10 +47,8 @@ class App extends Component {
     } else {
       data.latlng.key = oldMarkers[oldMarkers.length-1].key+1;
     }
-    console.log(this.state)
     oldMarkers.push(data.latlng)
     this.setState({markers: oldMarkers})
-    console.log(JSON.stringify(this.state.markers))
     fetch(`http://127.0.0.1:5000/`, {
       method: 'POST',
       headers: {
@@ -50,9 +58,7 @@ class App extends Component {
       body: JSON.stringify(this.state.markers)
     }).then(response => response.json())
       .then((responseJson) => {
-        console.log(responseJson);
         this.setState({tracts:responseJson});
-        console.log(this.state);
       });
   }
 
@@ -72,11 +78,7 @@ class App extends Component {
       }
     }
     this.addMarker(data);
-    this.setState({
-      lat: suggest.location.lat,
-      lng: suggest.location.lng,
-      zoom: 16
-    });
+    this.zoomToCoordinates(suggest.location.lat, suggest.location.lng)
   }
 
   render () {
@@ -108,7 +110,7 @@ class App extends Component {
         </Map>
         <div className="panels">
           <SearchBox onSuggestSelect={this.onSuggestSelect.bind(this)}/>
-          <ResultsBox tracts={this.state.tracts}/>
+          <ResultsBox tracts={this.state.tracts} zoomer= {this.zoomToCoordinates.bind(this)}/>
         </div>
       </div>
     )
@@ -152,7 +154,7 @@ class Result extends Component {
 
   render() {
     return (
-      <div>
+      <div onClick={this.props.zoomer.bind(this, this.props.center_lat, this.props.center_lng)}>
         <img width='80' height='92' src={this.props.img_src}/>
         <b>{this.props.name}</b>
         <div>
@@ -177,11 +179,14 @@ class ResultsBox extends Component {
     const Results = this.props.tracts.map(tract => (
       <Result
         name={tract.tract.name}
+        center_lat={tract.tract.center_lat}
+        center_lng={tract.tract.center_lng}
         img_src={tract.tract.img_src}
         transportation={tract.tract.transportation_rank}
         education={tract.tract.education_rank}
         wellness={tract.tract.wellness_rank}
-        connectivity={tract.tract.connectivity_rank}/>
+        connectivity={tract.tract.connectivity_rank}
+        zoomer={this.props.zoomer}/>
     ));
     return (
       <div>
